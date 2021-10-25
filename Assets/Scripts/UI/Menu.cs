5c4337d13030bfd106d8f0945a80510be8a12963
aaaXorn <lcrs.rp@gmail.com>
2021-10-25 10:53:53 -0300

    opção de volume do main menu

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class Menu : MonoBehaviour
 {
+	//script de transição
 	[SerializeField] SceneTransition ST;
+	//mixer de audio
+	[SerializeField] AudioMixer audioM;
+	
+	//objeto com as configurações
+	[SerializeField] GameObject OptionsObj;
 	
     // Start is called before the first frame update
     void Start()
@@ -26,13 +33,26 @@ public class Menu : MonoBehaviour
 			//nível escolhido
 	}
 	
+	#region options
+	
 	public void Options()
 	{
-		//abre as opções
+		//abre/fecha as configurações
+		OptionsObj.SetActive(!OptionsObj.activeSelf);
 	}
+		
+		//configura o master volume de acordo com um slider
+		public void VolumeSlider(float volume)
+		{
+			//setta a variável de volume do AudioMixer
+			audioM.SetFloat("Volume", Mathf.Log10(volume) * 20);
+		}
+	
+	#endregion
 	
 	public void Exit()
 	{
+		//sai do jogo
 		Application.Quit();
 	}
 }
