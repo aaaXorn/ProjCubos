98a7a56e0519527b5309534cf7beb4fdab0379bd
aaaXorn <lcrs.rp@gmail.com>
2021-10-30 16:47:10 -0300

    debugging do novo grab, comentários adicionados pra facilitar a compreensão

--- Assets/Scripts/Player/Pickup_Teste.cs
+++ Assets/Scripts/Player/Pickup_Teste.cs
@@ -4,55 +4,60 @@ using UnityEngine;
 
 public class Pickup_Teste : MonoBehaviour
 {
-
+	//input de grab
     [SerializeField]
     bool grabInput;
 
+	//o que o raycast atingiu
     RaycastHit rayHit;
     
+	//alcance do raycast, onde o raycast acertou e o quão pra baixo o raycast é gerado
     [SerializeField]
-    float raycastDist, hitDist;
+    float raycastDist, hitDist, rayDownMod;
 
+	//layer que o pickup deve afetar
     [SerializeField]
-    string layerMask;
-    int groundLayer;
+    string layerMask;//nome da layer
+    int groundLayer;//id da layer
 
     
     // Start is called before the first frame update
     void Start()
     {
+		//setta o groundLayer como a layer que tem o nome layerMask
         groundLayer = LayerMask.GetMask(layerMask);
     }
 
     // Update is called once per frame
     void Update()
     {
+		//setta o input de grab
         grabInput = Input.GetButton("Interact");
-        Debug.Log(grabInput);
+        Debug.Log("grab " + grabInput);
 
+		//debug: desenha o Raycast na tela, Vector3.right em vez de .forward porque a frente ta como X em vez de Z ops
+		Debug.DrawRay(transform.position - (rayDownMod * Vector3.up), transform.TransformDirection(Vector3.right) * raycastDist, Color.red);
+
+		//se o jogador está tentando pegar um item
         if (grabInput)
         {
-           
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out rayHit, raycastDist, groundLayer))
+			//gera um raycast, se acertar um objeto da layer layerMask continua
+            if (Physics.Raycast(transform.position - (rayDownMod * Vector3.up), transform.TransformDirection(Vector3.right), out rayHit, raycastDist, groundLayer))
             {
+				//checa a distância entre o jogador e o objeto atingido
                 hitDist = Vector3.Distance(transform.position, rayHit.point);
-
+				
+				//se a distância for menor que 2
                 if (hitDist < 2)
                 {
-
+					Debug.Log("perto");
                 }
-                   else
+				//se for maior que 2
+                else
                 {
-
+					Debug.Log("longe");
                 }            
             }
-        } 
-            
+        }
     }
-
-    void FixedUpdate()
-    {
-
-    }
-
 }
