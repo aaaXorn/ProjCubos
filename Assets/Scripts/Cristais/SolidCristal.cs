ca4416bc62781dba4b8014b594f2b127d0921fdf
aaaXorn <lcrs.rp@gmail.com>
2021-11-09 10:08:13 -0300

    partículas de efeito pra quando o player pega um cristal

--- Assets/Scripts/Cristais/SolidCristal.cs
+++ Assets/Scripts/Cristais/SolidCristal.cs
@@ -8,13 +8,16 @@ public class SolidCristal : MonoBehaviour
 	[SerializeField] Image CSolidoUI;
 	[SerializeField] GameObject Gate;//leva pro próximo nível
 	
+	[SerializeField] GameObject Particles;//partículas criadas quando o cristal é destruído
+	
     void OnTriggerEnter(Collider other)
 	{
 		//deixa o jogador ir pro próximo nível
 		Gate.SetActive(true);
 		
-		//habilita o sprite e destrói o cristal
+		//habilita o sprite, cria partículas e destrói o cristal
 		CSolidoUI.enabled = true;
+		Instantiate(Particles, transform.position, transform.rotation);
 		Destroy(gameObject);
 	}
 }
