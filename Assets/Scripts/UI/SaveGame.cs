5e5c7f2404ac1a20df5341cda43dc6c689d39a63
aaaXorn <lcrs.rp@gmail.com>
2021-11-18 10:39:18 -0300

    backup porque Unity

--- Assets/Scripts/UI/SaveGame.cs
+++ Assets/Scripts/UI/SaveGame.cs
@@ -10,8 +10,8 @@ public class SaveGame : MonoBehaviour
 	public static SaveGame instance {get; private set;}//instância do SaveGame
 	string path;//local do arquivo de save
 	
-	//língua do texto do jogo, 0 english 1 portugues
-	public int language;
+	//língua do texto do jogo
+	public string language;
 	
 	//níveis que o player pode jogar com Load Level
 	public int levelsUnlocked;
@@ -87,7 +87,7 @@ public class SaveGame : MonoBehaviour
 	[Serializable]
 	class SaveData
 	{
-		public int language;
+		public string language;
 		public int levelsUnlocked;
 		public int graphicQuality;
 		public bool fullScreen;
